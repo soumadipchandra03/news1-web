33f650ea11e154c24776ffdcfa810b5fb34614ad
soumadipchandra <142772709+soumadipchandra03@users.noreply.github.com>
2024-08-24 20:58:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -85,3 +85,9 @@ function updateDateTime() {
 }
 updateDateTime();
 setInterval(updateDateTime, 1000);
+const themeToggleButton = document.getElementById('theme-toggle');
+
+themeToggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('night-mode');
+});
+
